f833a21c05bac68e2c133f6b269dc0eeb57894fc
Jordan.B <jb.jokod@gmail.com>
2023-10-15 02:20:04 +0200

    👌 IMPROVE: Set the traductions of servers when init client

--- events/getInteraction.js
+++ events/getInteraction.js
@@ -1,23 +1,9 @@
 const { Events } = require('discord.js');
-const { Server } = require('../data/models.js');
 const Logs = require('../utils/logs.js');
 
 module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
-		const { client, guild } = interaction;
-		const guildId = guild.id;
-
 		new Logs(interaction).write();
-
-		const server = await Server.findOne({ guild_id: guildId });
-
-		const lang = server?.lang || 'en';
-
-		if (!server) {
-			console.log(`[ERROR] Server "${guildId}" not found or language not defined.`);
-		}
-
-		client.traductions.set(guildId, lang);
 	},
 };
